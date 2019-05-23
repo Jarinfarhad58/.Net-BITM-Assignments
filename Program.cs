e876b369cfc9574c6bab53f58e4f0a7e23f88940
Jarinfarhad58 <jarinfarhad.58@gmail.com>
2019-05-24 02:33:12 +0600

    Assignment1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+             string name= Console.ReadLine();
+            Console.WriteLine("What is your Address?");
+            string address = Console.ReadLine();
+            Console.WriteLine("What is your Mobile?");
+            string mobile = Console.ReadLine();
+            Console.WriteLine("What is your Date of Birth?");
+            string date = Console.ReadLine();
+            Console.WriteLine("What is your Age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Name:" +name);
+            Console.WriteLine("Address:" + address);
+            Console.WriteLine("Mobile:" + mobile);
+            Console.WriteLine("Date of Birth:" + date);
+            Console.WriteLine("Age:" + age);
+            Console.ReadKey();
+        }
+    }
+}
